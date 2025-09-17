6ec941fe9532aa42706ce8e47dd673f136445936
Maria Eduarda <mariaeduardareisTDS2.senai@gmail.com>
2025-09-17 13:55:13 -0300

    feat: add Prisma configuration and initial server structure

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors"; // Importa o middleware CORS
+
+// import routes from "./routes/index.routes.js";
+
+config(); // Carrega variáveis de ambiente do arquivo .env
+const port = process.env.PORT || 5000;
+
+// Inicializa o Express
+const app = express();
+app.use(cors()); // Habilita CORS para todas as rotas
+
+app.use(express.json()); // Parse de JSON
+
+// Rota base para verificar se o servidor está rodando
+app.get("/", (req, res) => {
+  res.json({ message: "API de autores e livros funcionando! 📖" });
+});
+
+app.use("/", routes);
+
+// Iniciar o servidor
+app.listen(port, () => {
+  console.log(`📖 Servidor rodando na porta ${port}`);
+});
